a91fe3c4b2a60ce82be90aa7024acaa1b317bab5
Sindhu.Rudraboina <sindhu.rudraboina@gmail.com>
2024-02-10 00:14:05 +0530

    Added cars to list and toString

--- src/CarPriceComparator.java
+++ src/CarPriceComparator.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class CarPriceComparator implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
-        o1.getPrice() - o2.getPrice();
+        return o1.getPrice() - o2.getPrice();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -5,5 +9,12 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Car class demo");
+        Car c1 = new Car(100000, 1200);
+        Car c2 = new Car(200000, 1000);
+        Car c3 = new Car(300000, 800);
+        Car c4 = new Car(400000, 600);
+        List<Car> carList = new ArrayList<>(Arrays.asList(c4, c2, c1, c3));
+        carList.sort(new CarPriceComparator());
+        System.out.println(carList);
     }
 }
